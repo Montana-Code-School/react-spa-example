9bd61aa487f7722b8b0b5f8420bcb165464a55b8
fresh5447 <doug@kosmojo.com>
2016-04-05 10:38:37 -0600

    name input change uses ref closure, deleteing after this commit

--- client/FishComps/EditFishData.js
+++ client/FishComps/EditFishData.js
@@ -1,17 +1,76 @@
 var React = require('react');
+var EditFishForm = require('./EditFishForm');
+
 
 var EditFishData = React.createClass({
-  //GET ONE FISHS DATA --> AJAX GET
-  //PASS THAT STATE INTO FORM
-  //USE FORM TO SHOW THAT DATA
+  /* 1) GET INITIAL STATE */
+
+  getInitialState: function() {
+    return {
+      name: null,
+      color: null,
+      people_eater: null,
+      length: null,
+      img: null
+    }
+  },
+
+  /* 2) LOAD A FISH FROM SERVER --> SET STATE */
+  loadOneFishFromServer: function() {
+    var self = this;
+    $.ajax({
+      url: '/api/fish/one_fish/' + this.props.id,
+      method: 'GET',
+    }).done(function(data){
+      self.setState({
+        name: data.name,
+        color: data.color,
+        people_eater: data.people_eater,
+        length: data.length,
+        img: data.img
+      })
+    })
+  },
+
+  onFieldChange: function(fieldName, fieldValue) {
+    const newState = {};
+    newState[fieldName] = fieldValue;
+    this.setState(newState);
+  },
+
+  onNameChange: function(event){
+    this.setState({ name: event.target.value })
+  },
+  onColorChange: function(event){
+    this.setState({ color: event.target.value })
+  },
+  onImgChange: function(event){
+    this.setState({ img: event.target.value })
+  },
+  onLengthChange: function(event){
+    this.setState({ length: event.target.value })
+  },
+  peopleEaterChange: function(event){
+    this.setState({ peopleEater: event.target.value })
+  },
+
+  componentDidMount: function() {
+    this.loadOneFishFromServer()
+  },
+
+  /* 3)PASS FISH DATA TO EDIT_FISH_FORM */
+
   render: function() {
-    return (
-      <div>
-        <h3> You have found EditFishData </h3>
-        <p> Fish Id is: { this.props.id } </p>
-      </div>
-      )
+    return this.state.name ? <EditFishForm
+                                onFieldChange={(...args) => this.onFieldChange(...args)} 
+                                peopleEaterChange={this.peopleEaterChange} 
+                                onLengthChange={this.onLengthChange} 
+                                onImgChange={this.onImgChange} 
+                                onColorChange={this.onColorChange} 
+                                onNameChange={this.onNameChange} 
+                                {...this.state} /> : null;
   }
+
 });
 
 module.exports = EditFishData;
---
+++ client/FishComps/EditFishForm.js
@@ -0,0 +1,41 @@
+var React = require('react');
+
+function EditFishForm(props) {
+    let nameInput;
+    return (
+      <div className="container myContainer">
+      <form>
+        <h3> Enter New Fish </h3>
+        <fieldset className="form-group">
+          <label>name</label>
+          <input ref={ (input) => nameInput = input } onChange={ () => props.onFieldChange('name', nameInput.value ) } value={props.name} type="text" className="form-control"/>
+        </fieldset>
+        <fieldset className="form-group">
+          <label>color</label>
+          <input onChange={props.onColorChange} value={props.color}  type="text" className="form-control"/>
+        </fieldset>
+        <fieldset className="form-group">
+          <label>length</label>
+          <input onChange={props.onLengthChange} value={props.length} type="text" className="form-control"/>
+        </fieldset>
+        <fieldset className="form-group">
+          <label>img src</label>
+          <input onChange={props.onImgChange} value={props.img}  type="text" className="form-control"/>
+        </fieldset>
+
+        <fieldset className="form-group">
+          <label htmlFor="exampleSelect1">Man Eater?</label>
+          <select onChange={ props.peopleEaterChange } value={props.people_eater} className="form-control">
+            <option>select something</option>
+            <option value={ true }>yes</option>
+            <option value={  false }>no</option>
+          </select>
+        </fieldset>
+
+        <button className="btn btn-success-outline" type="submit"> Submit </button>
+      </form>
+      </div>
+      )
+};
+
+module.exports = EditFishForm;
